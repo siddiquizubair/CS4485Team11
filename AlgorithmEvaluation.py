b4b9e352836e22e8aac9f7d26681381c5cfb7236
siddiquizubair <81281327+siddiquizubair@users.noreply.github.com>
2022-10-21 00:30:22 -0500

    Updated AlgorithmEvaluation.py

--- AlgorithmEvaluation.py
+++ AlgorithmEvaluation.py
@@ -1,3 +1,4 @@
+from select import select
 from chatterbot.logic import LogicAdapter
 from chatterbot.conversation import Statement
 import re
@@ -5,31 +6,36 @@ import re
 binarySearch = {
     "best": "O(1)",
     "average": "O(log n)",
-    "worst": "O(log n)"
+    "worst": "O(log n)",
+    "expl": "Binary search is a search algorithm that finds the position of a target value within a sorted array.<br>Binary search compares the target value to the middle element of the array. If they are not<br> equal, the half in which the target cannot lie is eliminated and the search continues on the remaining half,<br>again taking the middle element to compare to the target value, and repeating this until the target value is found.<br>If the search ends with the remaining half being empty, the target is not in the array."
 }
 
 bubbleSort = {
     "best": "O(n)",
     "average": "O(n^2)",
-    "worst": "O(n^2)"
+    "worst": "O(n^2)",
+    "expl": "Bubble sort is a simple sorting algorithm that repeatedly steps through the input list element by element,<br>comparing the current element with the one after it, swapping their values if needed.These passes<br> through the list are repeated until no swaps had to be performed during a pass, meaning that the list<br> has become fully sorted."
 }
 
 insertSort = {
     "best": "O(n)",
     "average": "O(n^2)",
-    "worst": "O(n^2)"
+    "worst": "O(n^2)",
+    "expl": "Insertion sort is a simple sorting algorithm that works similar to the way you sort<br> playing cards in your hands. The array is virtually split into a sorted and an unsorted part.<br> Values from the unsorted part are picked and placed at the correct position in the sorted part."
 }
 
 quickSort = {
     "best": "O(nlogn)",
     "average": "O(nlogn)",
-    "worst": "O(n^2)"
+    "worst": "O(n^2)",
+    "expl": "Quicksort is a divide-and-conquer algorithm. It works by selecting a 'pivot' element from <br>the array and partitioning the other elements into two sub-arrays, according to whether they<br> are less than or greater than the pivot. For this reason, it is sometimes called partition-exchange sort.<br>The sub-arrays are then sorted recursively."
 }
 
 selectionSort = {
     "best": "O(n^2)",
     "average": "O(n^2)",
-    "worst": "O(n^2)"
+    "worst": "O(n^2)",
+    "expl": "Selection sort divides the input list into two parts: a sorted sublist of items which is built up from left to <br>right at the front (left) of the list and a sublist of the remaining unsorted items that occupy the rest of<br> the list. Initially, the sorted sublist is empty and the unsorted sublist is the entire input list. The algorithm<br> proceeds by finding the smallest (or largest, depending on sorting order) element in the unsorted sublist, <br>exchanging (swapping) it with the leftmost unsorted element (putting it in sorted order), and moving the <br>sublist boundaries one element to the right."
 }
 
 class AlgorithmLogic(LogicAdapter):
@@ -45,9 +51,11 @@ class AlgorithmLogic(LogicAdapter):
         response = Statement(text = input_text)
         response.confidence = 0
 
-        bigO = re.findall("big o|big O|Big O|Big o", input_text)
+        bigO = re.findall("big o|big O|Big o|Big O", input_text)
         
         runtime = re.findall("runtime", input_text)
+
+        question = re.findall("what|What|how|How|work", input_text)
 
         if len(bigO) != 0:
             print("checking big o")
@@ -63,6 +71,24 @@ class AlgorithmLogic(LogicAdapter):
             response = Statement(text = self.checkSorts(input_text))
             if response is not None:
                 response.confidence = 1
+        elif len(question) != 0:
+            if "sort" in input_text:
+                if "bubble" in input_text or "Bubble" in input_text:
+                    response = Statement(text = bubbleSort["expl"])
+                    response.confidence = 1
+                elif "quick" in input_text or "Quick" in input_text:
+                    response = Statement(text = quickSort["expl"])
+                    response.confidence = 1
+                elif "selection" in input_text or "Selection" in input_text:
+                    response = Statement(text = selectionSort["expl"])
+                    response.confidence = 1
+                elif "insertion" in input_text or "Insertion" in input_text:
+                    response = Statement(text = insertSort["expl"])
+                    response.confidence = 1
+            elif "search" in input_text:
+                if "binary" in input_text or "Binary" in input_text:
+                    response = Statement(text = binarySearch["expl"])
+                    response.confidence = 1
 
         return response
 
@@ -73,6 +99,8 @@ class AlgorithmLogic(LogicAdapter):
                 return self.getRuntime(bubbleSort)
             if "quick" in algo:
                 return self.getRuntime(quickSort)
+            if "selection" in algo:
+                return self.getRuntime(selectionSort)
 
 
         if "search" in algo:
--- app.py
+++ app.py
@@ -15,7 +15,7 @@ algo_bot = ChatBot(
         {
             'import_path': 'chatterbot.logic.BestMatch',
             'default_response': 'I\'m sorry, but I don\'t understand your question. Please refer to your course on eLearning or contact Professor Chida.',
-            'maximum_similarity_threshold': 0.9
+            'maximum_similarity_threshold': 0.8
         }
     ],
     preprocessors = [
@@ -44,7 +44,28 @@ listTrainer.train([
 
 listTrainer.train([
     "I need help",
-    "I can try to help (with algorithms)."
+    "I can try to help (with algorithms)." 
+])
+
+listTrainer.train([
+    "Selection sort",
+    "Would you like to learn more about selection sort?",
+    "I would",
+    "Selection sort divides the input list into two parts: a sorted sublist of items which is built up from left to <br>right at the front (left) of the list and a sublist of the remaining unsorted items that occupy the rest of<br> the list. Initially, the sorted sublist is empty and the unsorted sublist is the entire input list. The algorithm<br> proceeds by finding the smallest (or largest, depending on sorting order) element in the unsorted sublist, <br>exchanging (swapping) it with the leftmost unsorted element (putting it in sorted order), and moving the <br>sublist boundaries one element to the right."
+])
+
+listTrainer.train([
+    "Bubble sort",
+    "Would you like to learn more about bubble sort?",
+    "I would",
+    "Bubble sort is a simple sorting algorithm that repeatedly steps through the input list element by element,<br>comparing the current element with the one after it, swapping their values if needed.These passes<br> through the list are repeated until no swaps had to be performed during a pass, meaning that the list<br> has become fully sorted."
+])
+
+listTrainer.train([
+    "Quicksort",
+    "Would you like to learn more about quicksort?",
+    "I would",
+    "Quicksort is a divide-and-conquer algorithm. It works by selecting a 'pivot' element from <br>the array and partitioning the other elements into two sub-arrays, according to whether they<br> are less than or greater than the pivot. For this reason, it is sometimes called partition-exchange sort.<br>The sub-arrays are then sorted recursively."
 ])
 
 @app.route("/")
@@ -58,4 +79,4 @@ def get_response():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
